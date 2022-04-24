335bdfc4f029e1585806bc27cb35e3b107bdccdd
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2022-04-24 16:27:58 +0000

    Fixed launch.sh for Polybar, edited i3 config.
    Now Polybar launches correctly.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-killall -q polybar
+pkill -9 polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar mybar &

0e57c50290722af573689333a2da07ebcc7017e5
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2022-04-26 09:02:21 +0200

    Corrections to install.sh
    Small correction. Still not ready.

--- install.sh
+++ install.sh
@@ -11,13 +11,16 @@ function newDir () {
 }
 
 echo "Void Rice - by Lorenzo Barretta."
+echo
 
 HOME_DIR=$HOME
-RICE_DIR=$HOME_DIR
+RICE_DIR=$(pwd)
 
 echo "Home Directory: $HOME_DIR"
+echo
 echo "The default location for the Void-Rice repository is $RICE_DIR"
 read -p "Is this correct? (y/n) " CHOICE
+echo
 
 case "$CHOICE" in
 	y|Y) defaultDir;;
